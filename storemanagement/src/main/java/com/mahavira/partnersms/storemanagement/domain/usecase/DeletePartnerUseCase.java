d29fc8a8c14543a2d3150348a1fcc5ad1e96f262
Norman Lie <norman.lie@kudo.co.id>
2018-07-15 21:48:28 +0700

    Add entity, repo, and use case for partner management

---
+++ storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/domain/usecase/DeletePartnerUseCase.java
@@ -0,0 +1,29 @@
+package com.mahavira.partnersms.storemanagement.domain.usecase;
+
+import com.mahavira.partnersms.base.core.CompletableUseCase;
+import com.mahavira.partnersms.storemanagement.domain.entitiy.Partner;
+import com.mahavira.partnersms.storemanagement.domain.repo.PartnerRepository;
+
+import javax.inject.Inject;
+
+import io.reactivex.Completable;
+
+/**
+ * Created by norman on 15/07/18.
+ *
+ */
+
+public class DeletePartnerUseCase implements CompletableUseCase<Partner> {
+
+    private PartnerRepository mRepository;
+
+    @Inject
+    DeletePartnerUseCase(PartnerRepository repository) {
+        mRepository = repository;
+    }
+
+    @Override
+    public Completable execute(Partner param) throws Exception {
+        return mRepository.deletePartner(param);
+    }
+}
