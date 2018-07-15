8defd07b8e466c5d1d97cd511707457180e443c2
Norman Lie <norman.lie@kudo.co.id>
2018-07-15 22:51:27 +0700

    add observer for add partner event

--- storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/addpartner/AddPartnerActivity.java
+++ storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/addpartner/AddPartnerActivity.java
@@ -3,10 +3,12 @@ package com.mahavira.partnersms.storemanagement.presentation.feature.addpartner;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.mahavira.partnersms.base.presentation.BaseActivity;
 import com.mahavira.partnersms.storemanagement.R;
 import com.mahavira.partnersms.storemanagement.databinding.ActivityAddPartnerBinding;
+import com.mahavira.partnersms.storemanagement.domain.entitiy.Partner;
 
 public class AddPartnerActivity extends BaseActivity<ActivityAddPartnerBinding, AddPartnerViewModel> {
 
@@ -23,6 +25,30 @@ public class AddPartnerActivity extends BaseActivity<ActivityAddPartnerBinding, 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        observeAddPartnerResult();
+        observeErrorMessage();
+    }
+
+    private void observeAddPartnerResult() {
+        getViewModel().getAddPartnerResult().observe(this, addPartner -> {
+            if (addPartner != null) {
+                switch (addPartner.status) {
+                    case SUCCESS:
+                        Toast.makeText(this, "Add Partner Success", Toast.LENGTH_SHORT).show();
+                        finish();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void observeErrorMessage() {
+        getViewModel().getErrorMessage().observe(this, errorMessage -> {
+            if (errorMessage != null) {
+                Toast.makeText(this, "Add Partner Failed, " + errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -33,8 +59,9 @@ public class AddPartnerActivity extends BaseActivity<ActivityAddPartnerBinding, 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_save) {
-
+        if (item.getItemId() == R.id.action_save) {
+            Partner partner = new Partner();
+            getViewModel().attemptAddPartner(partner);
         }
         return super.onOptionsItemSelected(item);
     }
--- storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/addpartner/AddPartnerViewModel.java
+++ storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/addpartner/AddPartnerViewModel.java
@@ -1,8 +1,18 @@
 package com.mahavira.partnersms.storemanagement.presentation.feature.addpartner;
 
+import android.arch.lifecycle.MutableLiveData;
+import android.databinding.ObservableField;
+
+import com.mahavira.partnersms.base.core.Resource;
 import com.mahavira.partnersms.base.presentation.BaseViewModel;
+import com.mahavira.partnersms.storemanagement.domain.entitiy.Partner;
+import com.mahavira.partnersms.storemanagement.domain.usecase.AddPartnerUseCase;
 
 import javax.inject.Inject;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by norman on 14/07/18.
@@ -11,8 +21,46 @@ import javax.inject.Inject;
 
 public class AddPartnerViewModel extends BaseViewModel {
 
-    @Inject
-    AddPartnerViewModel() {
+    public final ObservableField<Boolean> mShowLoading = new ObservableField<>(false);
 
+    private final MutableLiveData<Resource<Boolean>> mAddPartnerResult = new MutableLiveData<>();
+
+    private final MutableLiveData<String> mErrorMessage = new MutableLiveData<>();
+
+    private CompositeDisposable mDisposable = new CompositeDisposable();
+
+    private AddPartnerUseCase mAddPartnerUseCase;
+
+    @Inject
+    AddPartnerViewModel(AddPartnerUseCase addPartnerUseCase) {
+        mAddPartnerUseCase = addPartnerUseCase;
+    }
+
+    MutableLiveData<Resource<Boolean>> getAddPartnerResult() {
+        return mAddPartnerResult;
+    }
+
+    MutableLiveData<String> getErrorMessage() {
+        return mErrorMessage;
+    }
+
+    public void attemptAddPartner(Partner partner) {
+        mDisposable.add(mAddPartnerUseCase.execute(partner)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(__ -> onSubscribe())
+                .subscribe(this::onSuccess, this::onFailed));
+    }
+
+    private void onSubscribe() {
+        mShowLoading.set(true);
+    }
+
+    private void onFailed(Throwable throwable) {
+        mErrorMessage.setValue(throwable.getLocalizedMessage());
+    }
+
+    private void onSuccess() {
+        mAddPartnerResult.setValue(Resource.success(true));
     }
 }
