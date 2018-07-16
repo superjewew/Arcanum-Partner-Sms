613d53661f59e2a1ad0593989ff2598b2b1c4eec
Norman Lie <norman.lie@kudo.co.id>
2018-07-17 01:27:49 +0700

    continue implementing partner detail screen

--- storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/partnerdetail/PartnerDetailActivity.java
+++ storemanagement/src/main/java/com/mahavira/partnersms/storemanagement/presentation/feature/partnerdetail/PartnerDetailActivity.java
@@ -1,12 +1,22 @@
 package com.mahavira.partnersms.storemanagement.presentation.feature.partnerdetail;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.mahavira.partnersms.base.presentation.BaseActivity;
+import com.mahavira.partnersms.base.presentation.ExtraInjectable;
 import com.mahavira.partnersms.storemanagement.R;
 import com.mahavira.partnersms.storemanagement.databinding.ActivityPartnerDetailBinding;
+import com.mahavira.partnersms.storemanagement.domain.entitiy.Partner;
 
-public class PartnerDetailActivity extends BaseActivity<ActivityPartnerDetailBinding, PartnerDetailViewModel> {
+import org.parceler.Parcels;
+
+public class PartnerDetailActivity extends BaseActivity<ActivityPartnerDetailBinding, PartnerDetailViewModel>
+        implements ExtraInjectable{
+
+    public static final String PARTNER_EXTRA = "partner";
+
+    private Partner mPartner;
 
     @Override
     public int getViewModelBindingVariable() {
@@ -21,5 +31,14 @@ public class PartnerDetailActivity extends BaseActivity<ActivityPartnerDetailBin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getDataBinding().setPartner(mPartner);
+    }
+
+    @Override
+    public void injectExtras(@NonNull Bundle extras) {
+        if(extras.containsKey(PARTNER_EXTRA)) {
+            mPartner = Parcels.unwrap(extras.getParcelable(PARTNER_EXTRA));
+        }
     }
 }
