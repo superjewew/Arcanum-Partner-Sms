6c9cf287455467cf666e2ed8932ee53f650fe7dd
Norman Lie <norman.lie@kudo.co.id>
2018-07-18 11:59:38 +0700

    add router implementation for product list

--- app/src/main/java/com/mahavira/partnersms/router/InventoryRouterImpl.java
+++ app/src/main/java/com/mahavira/partnersms/router/InventoryRouterImpl.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import com.mahavira.partnersms.inventory.presentation.InventoryRouter;
 import com.mahavira.partnersms.inventory.presentation.addproduct.AddProductActivity;
+import com.mahavira.partnersms.inventory.presentation.getproducts.ProductListActivity;
 
 /**
  * Created by norman on 17/07/18.
@@ -15,6 +16,12 @@ public class InventoryRouterImpl implements InventoryRouter {
     @Override
     public void goToAddProduct(Context context) {
         Intent intent = new Intent(context, AddProductActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    public void goToProductList(Context context) {
+        Intent intent = new Intent(context, ProductListActivity.class);
         context.startActivity(intent);
     }
 }
