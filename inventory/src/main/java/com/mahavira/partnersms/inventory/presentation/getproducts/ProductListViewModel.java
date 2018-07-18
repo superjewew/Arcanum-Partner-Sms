b877c5dc729c4c95881cdf4dbf05270882e76740
Norman Lie <norman.lie@kudo.co.id>
2018-07-18 11:46:44 +0700

    use get product usecase in viewmodel

--- inventory/src/main/java/com/mahavira/partnersms/inventory/presentation/getproducts/ProductListViewModel.java
+++ inventory/src/main/java/com/mahavira/partnersms/inventory/presentation/getproducts/ProductListViewModel.java
@@ -1,8 +1,20 @@
 package com.mahavira.partnersms.inventory.presentation.getproducts;
 
+import android.arch.lifecycle.MutableLiveData;
+import android.databinding.ObservableBoolean;
+
+import com.mahavira.partnersms.base.core.Resource;
 import com.mahavira.partnersms.base.presentation.BaseViewModel;
+import com.mahavira.partnersms.inventory.domain.entity.Boardgame;
+import com.mahavira.partnersms.inventory.domain.usecase.GetProductsUseCase;
+
+import java.util.List;
 
 import javax.inject.Inject;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by norman on 18/07/18.
@@ -11,8 +23,37 @@ import javax.inject.Inject;
 
 public class ProductListViewModel extends BaseViewModel {
 
-    @Inject
-    ProductListViewModel() {
+    public final ObservableBoolean mShowLoading = new ObservableBoolean();
 
+    private final MutableLiveData<Resource<List<Boardgame>>> mProductListData = new MutableLiveData<>();
+
+    private final CompositeDisposable mDisposable = new CompositeDisposable();
+
+    private GetProductsUseCase mGetProductsUseCase;
+
+    @Inject
+    ProductListViewModel(GetProductsUseCase getProductsUseCase) {
+        mGetProductsUseCase = getProductsUseCase;
+    }
+
+    void attemptGetProducts() {
+        mDisposable.add(mGetProductsUseCase.execute()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnSubscribe(__ -> onSubscribe())
+        .subscribe(this::onSuccess, this::onFailed));
+    }
+
+    private void onFailed(Throwable throwable) {
+        mProductListData.setValue(Resource.error(null, throwable.getLocalizedMessage(), null));
+    }
+
+    private void onSuccess(List<Boardgame> boardgames) {
+        mShowLoading.set(false);
+        mProductListData.setValue(Resource.success(boardgames));
+    }
+
+    private void onSubscribe() {
+        mShowLoading.set(true);
     }
 }
