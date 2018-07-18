9f0cbcfc0ff7abbd11863300a0ad23d09f002f9f
Norman Lie <norman.lie@kudo.co.id>
2018-07-18 11:33:58 +0700

    create adapter for product list

---
+++ inventory/src/main/java/com/mahavira/partnersms/inventory/presentation/getproducts/ProductListAdapter.java
@@ -0,0 +1,65 @@
+package com.mahavira.partnersms.inventory.presentation.getproducts;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.mahavira.partnersms.inventory.databinding.ItemProductListBinding;
+import com.mahavira.partnersms.inventory.domain.entity.Boardgame;
+
+import java.util.List;
+
+/**
+ * Created by norman on 18/07/18.
+ *
+ */
+
+public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ProductListViewHolder> {
+
+    private List<Boardgame> mProductList;
+
+    private Context mContext;
+
+    ProductListAdapter(Context context, List<Boardgame> products) {
+        mContext = context;
+        mProductList = products;
+    }
+
+    @NonNull
+    @Override
+    public ProductListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        ItemProductListBinding binding = ItemProductListBinding.inflate(inflater, parent, false);
+
+        return new ProductListViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductListViewHolder holder, int position) {
+        Boardgame product = mProductList.get(position);
+        holder.bind(product);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProductList.size();
+    }
+
+    class ProductListViewHolder extends RecyclerView.ViewHolder {
+
+        ItemProductListBinding mBinding;
+
+        ProductListViewHolder(ItemProductListBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+
+        public void bind(Boardgame product) {
+            mBinding.setProduct(product);
+            mBinding.executePendingBindings();
+        }
+    }
+}
